5f97101684edf72d9e7a6639827631622b0f7b61
fatmaerciyas <fatmaerciyas22@gmail.com>
2023-10-09 15:11:49 +0300

    single-page-app

---
+++ trip-app/src/pages/Homepage.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Homepage.module.css";
+
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <NavLink to="/app" className="cta">
+          Start
+        </NavLink>
+      </section>
+    </main>
+  );
+}
